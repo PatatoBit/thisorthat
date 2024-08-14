38c080159937478b127d68e58296d9a0dbf77d0c
Patato <pathonscript@gmail.com>
2024-08-14 22:19:06 +0700

    basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page">
+	<div class="container">
+		{#each [0, 1] as item}
+			<!-- content here -->
+			<div class="side">
+				<input type="file" accept="image/png, image/jpeg" />
+				<textarea placeholder="Custom description"></textarea>
+				<p>Descriptions after prompt...</p>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		margin-top: 5rem;
+	}
+
+	textarea {
+		resize: none;
+	}
+</style>
