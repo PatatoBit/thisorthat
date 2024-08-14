5b4026f845e49d9754bf9a1dae34a0ff93ccfe6b
Patato <pathonscript@gmail.com>
2024-08-14 22:24:20 +0700

    clean up styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,3 @@
-<div class="page">
-	<div class="container">
-		{#each [0, 1] as item}
-			<!-- content here -->
-			<div class="side">
-				<input type="file" accept="image/png, image/jpeg" />
-				<textarea placeholder="Custom description"></textarea>
-				<p>Descriptions after prompt...</p>
-			</div>
-		{/each}
-	</div>
-</div>
+<h1>Welcome</h1>
 
-<style lang="scss">
-	.container {
-		margin-top: 5rem;
-	}
-
-	textarea {
-		resize: none;
-	}
-</style>
+<a href="/app">App</a>
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,22 @@
+<div class="page">
+	<div class="container">
+		{#each [0, 1] as item}
+			<!-- content here -->
+			<div class="side">
+				<input type="file" accept="image/png, image/jpeg" multiple />
+				<textarea placeholder="Custom description"></textarea>
+				<p>Descriptions after prompt...</p>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		margin-top: 5rem;
+	}
+
+	textarea {
+		resize: none;
+	}
+</style>
