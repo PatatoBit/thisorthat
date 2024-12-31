697b20fe2bda3be93fc4cc697904d506aee2859c
Patato <pathonscript@gmail.com>
2024-12-31 15:47:26 +0700

    minimal contact page

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,7 @@
+<main class="container">
+	<h1>Contacts</h1>
+
+	<p>You can contact us via email</p>
+
+	<p><strong>Pat</strong> (dev): <a href="mailto:pathonscript@gmail.com">Email</a></p>
+</main>
